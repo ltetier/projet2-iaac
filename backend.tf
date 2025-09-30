93cc40318eb5c839099402cfa21def9cfb409070
ltetier <153027218+ltetier@users.noreply.github.com>
2025-09-30 11:44:08 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket       = "mon-tfstate-bucket-projet1-session-12345"
-    key          = "project1-iaac/main/terraform.tfstate"
-    region       = "eu-west-3"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket       = "mon-tfstate-bucket-projet1-session-12345"
+#    key          = "project1-iaac/main/terraform.tfstate"
+#    region       = "eu-west-3"
+#    use_lockfile = true
+#    encrypt      = true
+#  }
+#}
